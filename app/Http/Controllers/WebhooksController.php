ba3fab904b265f32c02d8ccc4c37bf3f8d2e7ee9
Carlos Hernandez <honchkrow1995@gmail.com>
2021-01-18 18:57:38 -0600

    Store Webhooks Logs to DB

--- app/Http/Controllers/WebhooksController.php
+++ app/Http/Controllers/WebhooksController.php
@@ -2,37 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Logs;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class WebhooksController extends Controller
 {
-    private $signingKey = 'ff174422be1893aa44c024b86aeb90d94fcff4c0daa349e700a753553fa5b364';
+
     //
     public function orderCreated(Request $request) {
         $data = $request->all();
         $payload = $request->getContent();
         $now = time(); // current unix timestamp
         $json = json_encode($payload, JSON_FORCE_OBJECT);
-        $signature = hash_hmac('sha256', $now.'.'.$json, $this->signingKey);
+        $signature = hash_hmac('sha256', $now.'.'.$json, env('BOLD_API_SIGNING_KEY'));
         $data['headers']['X-Bold-Signature'] = $request->header('X-Bold-Signature');
         $data['headers']['timestamp'] = $request->header('timestamp');
         $equals = hash_equals($data['headers']['X-Bold-Signature'], $signature);
         $data['match'] = $equals;
-        Log::error("Webhook Received>>" . $payload);
-        // todo add secure lines & DB store instead logs
-        // https://docs.boldapps.net/subscriptions/integration/index.html#securing-webhooks
+        Log::error("Order Received>>" . $payload);
+        Logs::create([
+            'type' => 2,
+            'raw' => $payload
+        ]);
     }
 
     public function subscriptionCreated(Request $request) {
         $payload = $request->getContent();
         $now = time(); // current unix timestamp
         $json = json_encode($payload, JSON_FORCE_OBJECT);
-        $signature = hash_hmac('sha256', $now.'.'.$json, $this->signingKey);
+        $signature = hash_hmac('sha256', $now.'.'.$json, env('BOLD_API_SIGNING_KEY'));
         $data['headers']['X-Bold-Signature'] = $request->header('X-Bold-Signature');
         $data['headers']['timestamp'] = $request->header('timestamp');
         $equals = hash_equals($data['headers']['X-Bold-Signature'], $signature);
         $data['match'] = $equals;
         Log::error("Subscription Received>>" . $payload);
+        Logs::create([
+            'type' => 1,
+            'raw' => $payload
+        ]);
     }
 }
